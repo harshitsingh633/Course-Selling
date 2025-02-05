b62fde516ef1a5995cae8ab20de412d82cf1312c
Harshit singh <hs8700882@gmail.com>
2025-02-05 20:02:49 +0530

    Updating bg Color

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import { NavBar } from "./components/NavBar"; 
 import React from "react";
 function App() {
-  return <div>
+  return <div className="w-full h-screen bg-[#FFFFFF]">
   <NavBar/>
+    <h1 className="text-[10vw] leading-none tracking-tight">100xDevs</h1>
 </div>
 }
 
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,13 +2,13 @@ import React from "react";
 
 export function NavBar() {
   return (
-    <div className="bg-slate-300 flex justify-evenly">
+    <div className="bg-[#FFFFFF] flex justify-evenly">
       <div><img src="https://appx-wsb-gcp-mcdn.akamai.net.in/subject/2023-01-17-0.17044360120951185.jpg" alt="100xDevs" className="h-10"/></div>
       <div>
         <input type="text" title="Search"/>
       </div>
       <div>
-        <button className="rounded bg-blue-400 p-3 m-1 ">SignUp</button>
+        <button className="rounded  bg-blue-400 p-3 m-1 ">SignUp</button>
       </div>
       <div>
         <button className="rounded bg-blue-400 p-3 m-1">Login</button>
