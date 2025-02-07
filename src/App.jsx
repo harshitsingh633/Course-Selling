b98415a9fc520d894f599252556a0fab1314ae04
Harshit singh <hs8700882@gmail.com>
2025-02-07 23:55:56 +0530

    Still working on NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function App() {
   return <div className="w-full h-screen bg-[#FFFFFF]">
   <NavBar/>
-    <h1 className="text-[10vw] leading-none tracking-tight">100xDevs</h1>
+      
 </div>
 }
 
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,16 +2,16 @@ import React from "react";
 
 export function NavBar() {
   return (
-    <div className="bg-[#FFFFFF] flex justify-evenly">
+    <div className="bg-[#FFFFFF] flex ">
       <div><img src="https://appx-wsb-gcp-mcdn.akamai.net.in/subject/2023-01-17-0.17044360120951185.jpg" alt="100xDevs" className="h-10"/></div>
-      <div>
-        <input type="text" title="Search"/>
+      <div className="p-3 m-1 border-black pl-[1000px] ">
+        <input type="text" title="Search" className="bg-blue-300 border-b-2 to-black"/>
       </div>
       <div>
-        <button className="rounded  bg-blue-400 p-3 m-1 ">SignUp</button>
+        <button className="rounded  bg-blue-400 p-3 m-1 ml-24">SignUp</button>
       </div>
       <div>
-        <button className="rounded bg-blue-400 p-3 m-1">Login</button>
+        <button className="rounded bg-blue-400 p-3 m-1 ml-10">Login</button>
       </div>
     </div>
   )
