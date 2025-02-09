de2564c59070d5520f7aeeb5ffe48d68ff1dd5d8
Harshit singh <hs8700882@gmail.com>
2025-02-09 23:46:58 +0530

    Updated Navbar and Created a SideBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import { NavBar } from "./components/NavBar"; 
 import React from "react";
+import SideBar from "./components/SideBar";
 function App() {
   return <div className="w-full h-screen bg-[#FFFFFF]">
   <NavBar/>
-      
+  <SideBar/>
 </div>
 }
 
---
+++ src/components/SideBar.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function SideBar() {
+  return (
+    <div className=' h-full w-[18%] bg-orange-200 flex text-gray-600 '>
+       <div>
+        <p className='flex pl-7 pt-10 text-sm'>
+            MAIN MENU
+        </p>
+        <button className='pl-7 pt-6 text-lg'>
+            Home
+        </button> 
+        <br />
+        <button className='pl-7 pt-6 text-lg'>
+            Courses
+        </button>
+       </div>
+
+    </div>
+  )
+}
+
+export default SideBar
