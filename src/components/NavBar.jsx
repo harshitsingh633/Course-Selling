0146bb22514ac0a2d9c75cef71cafe352ae2f27b
Harshit singh <hs8700882@gmail.com>
2025-02-10 09:58:44 +0530

    improved version

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,32 +2,30 @@ import React from "react";
 
 export function NavBar() {
   return (
-    <div className="bg-[#FFFFFF] flex w-full h-14 ">
-      <div className="flex ml-7 pt-2">
-        <img
-          src="https://appx-wsb-gcp-mcdn.akamai.net.in/subject/2023-01-17-0.17044360120951185.jpg"
-          alt="100xDevs"
-          className="h-10"
-        />
+    <div className="bg-[#FFFFFF] items-center flex w-full h-14 px-7">
+      <img
+        src="https://appx-wsb-gcp-mcdn.akamai.net.in/subject/2023-01-17-0.17044360120951185.jpg"
+        alt="100xDevs"
+        className="h-10"
+      />
+
+      <div className="flex-grow mx-5">
+        <div className="relative">
+          <input
+            type="text"
+            placeholder="Search here..."
+            className="w-full bg-gray-100 rounded-full py-2 px-4 pr-10 text-gray-700 placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-blue-400"
+          />
+          <button className="absolute right-3 top-2 text-gray-500">🔍</button>
+        </div>
       </div>
-      <div className="p-3 m-1 border-solid pl-[1000px] ">
-        <input
-          type="text"
-          title="Search"
-          placeholder="Type here to search.."
-          className="bg-transparent outline-2 outline-gray-700 flex-1 px-2 text-gray-700 placeholder-gray-400 "
-        />
-      </div>
-      <div className="pt-2">
-        <button className="rounded-lg  bg-blue-400 p-2 m-1 ml-22 text-slate-50 font-semibold">
-          SignUp
-        </button>
-      </div>
-      <div className="pt-2">
-        <button className="rounded-lg bg-blue-400 p-2 m-1 ml-10 text-slate-50 font-semibold">
-          Login
-        </button>
-      </div>
+
+      <button className="rounded-lg bg-blue-500 px-4 py-2 text-white font-semibold mx-2 hover:bg-blue-600">
+        Sign Up
+      </button>
+      <button className="rounded-lg bg-blue-500 px-4 py-2 text-white font-semibold hover:bg-blue-600">
+        Login
+      </button>
     </div>
   );
 }
